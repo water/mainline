a0b3a50191fbba38c381cbdd433cb2aff1cf68a5
August Lilleaas <august.lilleaas@gmail.com>
2010-06-01 01:18:30 +0200

    We have to specify a user id for the route to match. We had to do this in Rails 2 as well, so how this test ever passed is beyond me.

--- test/functional/licenses_controller_test.rb
+++ test/functional/licenses_controller_test.rb
@@ -24,37 +24,37 @@ class LicensesControllerTest < ActionController::TestCase
       @user = users(:old_timer)
       login_as :old_timer
     end
-    
+
     should "GET show redirect to edit with a flash" do
-      get :show
+      get :show, :user_id => @user.id
       assert_response :redirect
       assert_match(/You need to accept the/, flash[:notice])
     end
     
     should 'render the current license version if this has been accepted' do
       @user.accept_terms!
-      get :edit
+      get :edit, :user_id => @user.id
       assert_redirected_to :action => :show
     end
     
     should 'ask the user to confirm a newer version if this has not been acccepted' do
-      get :edit
+      get :edit, :user_id => @user.id
       assert_response :success
     end
     
     should 'require the user to accept the terms' do
-      put :update, :user => {}
+      put :update, :user => {}, :user_id => @user.id
       assert_redirected_to :action => :edit
     end
     
     should 'change the current version when selected' do
-      put :update, :user => { :terms_of_use => "1" }
+      put :update, :user => { :terms_of_use => "1" }, :user_id => @user.id
       assert_redirected_to :action => :show
       assert @user.reload.terms_accepted?
     end
     
     should "not change the current version if not selected" do
-      put :update, :user => {:terms_of_use => ""}
+      put :update, :user => {:terms_of_use => ""}, :user_id => @user.id
       assert !@user.reload.terms_accepted?
       assert_match(/You need to accept the/, flash[:error])
     end
