64d06b75420eb70006679220d471463b8650783c
Linus Oleander <linus@oleander.nu>
2012-01-27 00:59:46 +0100

    Using log as default deprecation warning

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,7 +11,6 @@ Gitorious::Application.configure do
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
-  config.action_view.debug_rjs             = true
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
@@ -23,10 +22,6 @@ Gitorious::Application.configure do
   }
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :test
-  # ExceptionNotifier.exception_recipients = YAML.load_file(File.join(Rails.root, "config/gitorious.yml"))["exception_notification_emails"]
-  # onfig.cache_store = :mem_cache_store, ['localhost:11211'], { 
-  #   :namespace => 'ks1' 
-  # }
-
-  # SslRequirement.disable_ssl_check = true
+  
+  config.active_support.deprecation = :log
 end
