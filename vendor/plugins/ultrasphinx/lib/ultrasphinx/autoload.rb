92e2f0dbf6b4977fd5f635fa0e0b2fc255fbd1e3
Linus Oleander <linus@oleander.nu>
2012-01-27 02:11:31 +0100

    Migrate possible, if ultrasphinx is deactivated

--- vendor/plugins/ultrasphinx/lib/ultrasphinx/autoload.rb
+++ vendor/plugins/ultrasphinx/lib/ultrasphinx/autoload.rb
@@ -1,12 +1,13 @@
 
-require 'initializer'
-
-class Rails::Initializer
-
-  def after_initialize_with_ultrasphinx_configuration
-    after_initialize_without_ultrasphinx_configuration
-    Ultrasphinx::Configure.load_constants
-  end     
-  
-  alias_method_chain :after_initialize, :ultrasphinx_configuration
-end
+# require 'initializer' unless defined?(Rails::Initializer)
+# 
+# class Rails::Initializer
+# 
+#   def after_initialize_with_ultrasphinx_configuration
+#     after_initialize_without_ultrasphinx_configuration
+#     Ultrasphinx::Configure.load_constants
+#   end     
+#   
+#   alias_method_chain :after_initialize, :ultrasphinx_configuration
+# end
+# 
