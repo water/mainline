f3440d85422cdcca30ccf65f7f64a6df76b39abc
Linus Oleander <linus@oleander.nu>
2012-01-27 01:35:01 +0100

    Adding localized_dates as plugin

--- vendor/plugins/localized_dates/lib/core_ext/time.rb
+++ vendor/plugins/localized_dates/lib/core_ext/time.rb
@@ -14,8 +14,21 @@
   alias_method :to_s, :to_formatted_s
 end
 
-::Time.const_set('DATE_FORMATS', {
+class Object
+  def def_if_not_defined(const, value)
+    mod = self.is_a?(Module) ? self : self.class
+    mod.const_set(const, value) unless mod.const_defined?(const)
+  end
+
+  def redef_without_warning(const, value)
+    mod = self.is_a?(Module) ? self : self.class
+    mod.send(:remove_const, const) if mod.const_defined?(const)
+    mod.const_set(const, value)
+  end
+end
+
+::Time.redef_without_warning :DATE_FORMATS, {
   :db           => "%Y-%m-%d %H:%M:%S",
   :number       => "%Y%m%d%H%M%S",
   :rfc822       => "%a, %d %b %Y %H:%M:%S %z"
-})
+}
