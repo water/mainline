6aaa29ab2d0cdcebef3cd637ccfcf36dddadb68a
Linus Oleander <linus@oleander.nu>
2012-01-31 00:42:14 +0100

    Should not be a one-liner

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,14 @@
 require "colorize"
 
-attributes = { login: "thedude",  email: "admin@popfizzle.com",  password: "abc123",  password_confirmation: "abc123",  terms_of_use: "1", activated_at: Time.now, is_admin: true }
+attributes = {
+  login: "thedude",  
+  email: "admin@popfizzle.com",  
+  password: "abc123",  
+  password_confirmation: "abc123",  
+  terms_of_use: "1", 
+  activated_at: Time.now, 
+  is_admin: true
+}
 
 user = User.new(attributes)
 user.login = attributes[:login]
