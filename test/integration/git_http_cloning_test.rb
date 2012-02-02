9a1e6f8171eb2bc313118839d0bacab8f617acaa
Linus Oleander <linus@oleander.nu>
2012-02-02 03:07:59 +0100

    Use correct include path for test spec file

--- test/integration/git_http_cloning_test.rb
+++ test/integration/git_http_cloning_test.rb
@@ -15,7 +15,7 @@
 #   You should have received a copy of the GNU Affero General Public License
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #++
-require File.join(File.dirname(__FILE__), "..", "test_helper")
+require_relative "../test_helper"
 
 class GitHttpCloningTest < ActionController::IntegrationTest
   context 'Request with git clone' do
