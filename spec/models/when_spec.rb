a70952aca3e86713551e04c7ed6d0eec188dba50
Linus Oleander <linus@oleander.nu>
2012-02-04 23:35:08 +0100

    When should only contain valid study p.

--- spec/models/when_spec.rb
+++ spec/models/when_spec.rb
@@ -4,5 +4,10 @@ describe When do
       Factory.build(:when, year: 2001).should be_valid
       Factory.build(:when, year: 1).should_not be_valid
     end
+    
+    it "requires a study period > 0" do
+      Factory.build(:when, study_period: 0).should_not be_valid
+      Factory.build(:when, study_period: 1).should be_valid
+    end
   end
 end
