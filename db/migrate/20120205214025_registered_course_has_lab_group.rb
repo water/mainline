4520cc922473f105aeddd4bb82358e70ce4e70a4
Linus Oleander <linus@oleander.nu>
2012-02-05 23:14:20 +0100

    Opps, wrong name on table. Should be pluralis

--- db/migrate/20120205214025_registered_course_has_lab_group.rb
+++ db/migrate/20120205214025_registered_course_has_lab_group.rb
@@ -1,14 +1,15 @@
 class RegisteredCourseHasLabGroup < ActiveRecord::Migration
   def self.up
-    create_table :registered_course_has_lab_group, id: false do |t|
+    create_table :registered_course_has_lab_groups, id: false do |t|
       t.integer :registered_course_id
       t.integer :lab_group_id
     end
     
-    add_index :registered_course_has_lab_group, :registered_course_id
-    add_index :registered_course_has_lab_group, :lab_group_id
+    add_index :registered_course_has_lab_groups, :registered_course_id
+    add_index :registered_course_has_lab_groups, :lab_group_id
   end
 
   def self.down
+    drop_table :registered_course_has_lab_groups
   end
 end
