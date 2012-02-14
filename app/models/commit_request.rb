a8909fa4f93b19611785bdbe5ec751fcf5f97c72
Jonas Ängeslevä <anjonas@student.chalmers.se>
2012-02-14 17:58:39 +0100

    Add code to commit_request

--- app/models/commit_request.rb
+++ app/models/commit_request.rb
@@ -9,6 +9,26 @@ class CommitRequest < Struct.new(:options)
   # }
   # Called from a beanstalkd worker
   def perform!
-    # TODO: make it work
+
+    puts "hej"
+    # command = options["command"]
+    # user = options["user"]
+    # repo = options["repo"]
+    # branch = options["branch"]
+    # commit_message = options["commit_message"]
+    paths = options[:paths]
+    files = options[:files]
+    if not [paths, files].all? 
+      puts "paths and files are empty"
+    else
+      paths.each do  |node|
+        #path_file.push(node["from"], node["to"])
+      puts "moving directory"
+      end
+      files.each do |node|
+        #path_map.push(node["from"], node["to"])
+      puts "moving file"
+      end
+      end
   end
 end
