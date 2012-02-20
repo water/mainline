14698b9115b0b529880542f7a1afb7d5cfa980f4
Pontus <spontus@spontus.se>
2012-02-20 14:42:07 +0100

    Refactor commit_request_spec, fancy

--- spec/models/commit_request_spec.rb
+++ spec/models/commit_request_spec.rb
@@ -1,58 +1,45 @@
 
 describe CommitRequest do
   describe "validation" do 
-    it "validates a commitrequest" do
-        u = User.new({
-            :id => 1,
-            :email => 'test@example.com',
-            :password => 'test',
-            :password_confirmation => 'test',
-            :terms_of_use => "1"
-        })
-        u.login  = 'test'
-        u.save
-      value = {
-        command: "move",
-        user: 1,
-        repository: 123,
-        branch: "master",
-        commit_message: "A commit message",
-        files: [{
-          from: "path/to/file.txt",
-          to: "path/to/newfile.text"
-          }]
-      }
-      cr = CommitRequest.new(value)
-      cr.should be_valid
-      cr.should_receive(:enqueue)
-      cr.save
+#    u = User.new({
+#        :email => 'test@example.com',
+#        :password => 'test',
+#        :password_confirmation => 'test',
+#        :terms_of_use => "1"
+#    })
+    before (:each) do
+        @u = Factory.build(:user)
+        @u.save
+        @value = {
+            command: "move",
+            user: @u.id,
+            repository: 123,
+            branch: "master",
+            commit_message: "A commit message",
+            files: [{
+            from: "path/to/file.txt",
+            to: "path/to/newfile.text"
+            }]
+        }
     end
 
-    it "can fail to validate a commitrequest" do
-        u = User.new({
-            :id => 1,
-            :email => 'test@example.com',
-            :password => 'test',
-            :password_confirmation => 'test',
-            :terms_of_use => "1"
-        })
-        u.login  = 'test'
-        u.save
-      value = {
-        command: "fiskpinne",
-        user: 1,
-        repository: 123,
-        branch: "master",
-        commit_message: "A commit message",
-        files: [{
-          from: "path/to/file.txt",
-          to: "path/to/newfile.text"
-          }]
-      }
-      cr = CommitRequest.new(value)
-      cr.should_not be_valid 
-      cr.should_not_receive(:enqueue)
-      cr.save
-    end 
+    it "validates a commitrequest" do
+      @cr = CommitRequest.new(@value)
+      @cr.should be_valid
+      @cr.should_receive(:enqueue)
+      @cr.save
+    end
+    
+    describe "failing validations" do
+        after (:each) do
+          @cr = CommitRequest.new(@value)
+          @cr.should_not be_valid 
+          @cr.should_not_receive(:enqueue)
+          @cr.save
+        end
+        it "should fail with an invalid command" do
+          @value[:command] = "fiskpinne";
+        end 
+    end
   end
 end
