256db02668c999e7c7b87c0eca4df8eebb0ede7b
Arash Rouhani <miffoljud@gmail.com>
2012-02-22 18:51:31 +0100

    Make JSON response in controller, not model #26

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -2,7 +2,13 @@ class UploadsController < ApplicationController
   def upload
     file = params[:files].first
     # response needs to be array to prepare for multiple simultaneous uploads
-    response = [Upload.store(file)]
+    u = Upload.new(file)
+
+    debug = {
+      local_path: u.path # TODO: should not be shown to user
+    }
+
+    response = {name: file.original_filename, id: u.hash, debug: debug}
     respond_to do |format|
       format.html { render :json => response} # For testing purposes
       format.json { render :json => response}
