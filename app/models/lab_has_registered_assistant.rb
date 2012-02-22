b3c28524fa7236fb690d72c424210e417cf9aa4c
Jesper Josefsson <jesper.josefsson@gmail.com>
2012-02-22 12:20:24 +0100

    Correct camelCasing #16

--- app/models/lab_has_registered_assistant.rb
+++ app/models/lab_has_registered_assistant.rb
@@ -1,5 +1,5 @@
 class LabHasRegisteredAssistant < ActiveRecord::Base
-  belongs_to :AssistantRegisteredToGivenCourse
-  belongs_to :Lab
-  belongs_to :When
+  belongs_to :assistant_registered_to_given_course
+  belongs_to :lab
+  belongs_to :when
 end
