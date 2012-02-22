bcfa45b0d7c32209a6e01c62ee6af715cb4259d7
Arash Rouhani <miffoljud@gmail.com>
2012-02-22 18:14:59 +0100

    Add routing for commit_request

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Gitorious::Application.routes.draw do
   post "upload" => "uploads#upload"
   get "new_upload" => "uploads#new"
 
+  post "commit_requests/new" => "commit_requests#new", :as => :commit_request
+
   resources :submissions, :only => [:index, :show, :create, :new]
 
   extend Gitorious::RepositoryRoutes
