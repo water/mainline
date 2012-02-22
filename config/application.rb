15fc9940bc689da8a9abdab231e9d3630e3354ea
Jesper Josefsson <jesper.josefsson@gmail.com>
2012-02-22 18:17:06 +0100

    Bumped version number to expire assets

--- config/application.rb
+++ config/application.rb
@@ -44,7 +44,7 @@ module Gitorious
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
+    config.assets.version = '1.1'
     
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
