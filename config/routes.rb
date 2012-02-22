d63317bc0f97e93602808b35f0419de268b157be
Jesper Josefsson <jesper.josefsson@gmail.com>
2012-02-22 18:26:36 +0100

    Merge routes file

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,6 @@ Gitorious::Application.routes.draw do
   get "new_upload" => "uploads#new"
   post "commit_requests/new" => "commit_requests#new", :as => :commit_request
 
-  post "commit_requests/new" => "commit_requests#new", :as => :commit_request
-
   resources :submissions, :only => [:index, :show, :create, :new]
 
   extend Gitorious::RepositoryRoutes
