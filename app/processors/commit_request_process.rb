0a18482d340a273606feec5dfd251c2614cf8a94
Linus Oleander <linus@oleander.nu>
2012-02-29 00:48:01 +0100

    Implement #move

--- app/processors/commit_request_process.rb
+++ app/processors/commit_request_process.rb
@@ -21,18 +21,6 @@ class CommitRequestProcessor < ApplicationProcessor
   #  }]
   # }
   #
-  # @add {
-  #  command: "add",
-  #  user: 1,
-  #  repository: 123,
-  #  branch: "master",
-  #  commit_message: "A commit message",
-  #  files: [{
-  #    raw: !Binary,
-  #    to: "path/to/dir"
-  #  }]
-  # }
-  #
   # @remove {
   #   user: 1,
   #   command: "remove",
@@ -52,7 +40,7 @@ class CommitRequestProcessor < ApplicationProcessor
   def on_message(message)
     @options = JSON.parse(message)
     send(@options.delete("command"), @options)
-    git.commit(options["commit_message"])
+    git.commit(@options["commit_message"])
   end
 
   #
@@ -75,16 +63,46 @@ class CommitRequestProcessor < ApplicationProcessor
     end
   end
 
+  # @move = {
+  #  user: 1,
+  #  repository: 123,
+  #  branch: "master",
+  #  commit_message: "A commit message",
+  #  files: [{
+  #    from: "path/to/file.txt",
+  #    to: "path/to/new_file.txt"
+  #  }]
+  # }
+  def move(options)
+    options["files"].each do |source|      
+      puts source["to"]
+      puts source["from"]
+      puts git[source["to"]]
+      puts git[source["from"]].class
+      puts git[source["from"]].content
+      puts "--------"
+      git[source["to"]] = git[source["from"]]#.content
+      split = source["from"].split("/")
+      if split.one?
+        git.delete(split.first)
+      else
+        file = split.last
+        dir = split[0..-2]
+        git[dir.join("/")].delete(file)
+      end
+    end
+  end
+
   private
-  def repository
-    Repository.find(@options["repository"])
-  end
+    def repository
+      Repository.find(@options["repository"])
+    end
 
-  def user
-    User.find(@options["user"])
-  end
+    def user
+      User.find(@options["user"])
+    end
 
-  def git
-    @_git ||= Gash.new(repository.full_repository_path, @options["branch"])
-  end
+    def git
+      @_git ||= Gash.new(repository.full_repository_path, @options["branch"])
+    end
 end
--- spec/models/processors/commit_request_processor_spec.rb
+++ spec/models/processors/commit_request_processor_spec.rb
@@ -2,10 +2,11 @@ describe CommitRequestProcessor do
   let(:processor) { CommitRequestProcessor.new }
   let(:user) { Factory.create(:user) }
   let(:repository) { Factory.create(:repository, user: user, owner: user) }
+  let(:content) { `cd #{@destintation} && git show --name-status --format=fuller` }
 
   before(:each) do
     @destintation = Dir.mktmpdir
-    `cd #{@destintation} && git --bare init`
+    `cd #{@destintation} && git init`
     @options = {}
     @options[:add] = {
       command: "add",
@@ -29,11 +30,54 @@ describe CommitRequestProcessor do
   end
 
   after(:each) do
-    puts @destintation
-    #{}`rm -rf #{@destintation}`
+    `rm -rf #{@destintation}`
   end
 
-  it "should add file to repo" do
-    processor.on_message(@options[:add].to_json)
+  describe "#add" do
+    it "should add file to repo" do
+      processor.on_message(@options[:add].to_json)
+    end
+  end
+
+  def add_file(file, content)
+    Dir.chdir(@destintation) do
+      `echo #{content} > #{file} && git add . && git commit -m "Commit"`
+    end
+  end
+
+  describe "#move" do
+    before(:each) do
+      @options = {
+        command: "move",
+        user: user.id,
+        repository: repository.id,
+        branch: "master",
+        commit_message: "A commit message",
+        files: [{
+          from: "old",
+          to: "new"
+        }]
+      }
+    end
+
+    it "should move file in repo" do
+      add_file("old", "content")
+      processor.on_message(@options.to_json)
+      content.match(%r{D  old})
+      content.match(%r{A  new})
+    end
+
+    it "can handle folders" do
+      add_file("file", "content")
+      hash = {
+         files: [{
+          from: "file",
+          to: "d/e/f"
+        }]
+      }
+      processor.on_message(@options.merge(hash).to_json)
+      content.match(%r{D  file})
+      content.match(%r{A  d/e/f})
+    end
   end
 end
