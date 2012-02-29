34c135a7c892b5e1daf87bb74659987e5e6f789a
Linus Oleander <linus@oleander.nu>
2012-02-29 01:44:42 +0100

    Testing #add for branches

--- spec/models/processors/commit_request_processor_spec.rb
+++ spec/models/processors/commit_request_processor_spec.rb
@@ -2,24 +2,14 @@ describe CommitRequestProcessor do
   let(:processor) { CommitRequestProcessor.new }
   let(:user) { Factory.create(:user) }
   let(:repository) { Factory.create(:repository, user: user, owner: user) }
-  let(:content) { `cd #{@destintation} && git show --name-status --format=fuller` }
+
+  def content_for(branch = "master")
+    `cd #{@destintation} && git checkout #{branch} 2&> /dev/null && git show --name-status --format=fuller`
+  end
 
   before(:each) do
     @destintation = Dir.mktmpdir
     `cd #{@destintation} && git init`
-    @options = {}
-    @options[:add] = {
-      command: "add",
-      user: user.id,
-      repository: repository.id,
-      branch: "master",
-      commit_message: "A commit message",
-      files: [{
-        raw: "Raw data",
-        to: "path/file"
-      }]
-    }
-
     Repository.should_receive(:find).
       any_number_of_times.
       with(repository.id).
@@ -34,8 +24,45 @@ describe CommitRequestProcessor do
   end
 
   describe "#add" do
+    before(:each) do
+      @options = {
+        command: "add",
+        user: user.id,
+        repository: repository.id,
+        branch: "master",
+        commit_message: "A commit message",
+        files: [{
+          raw: "Raw data",
+          to: "path/file"
+        }]
+      }
+    end
+
     it "should add file to repo" do
-      processor.on_message(@options[:add].to_json)
+      processor.on_message(@options.to_json)
+    end
+
+    it "can commit to other branches" do
+      processor = CommitRequestProcessor.new 
+      processor.on_message(@options.merge({
+        branch: "stable",
+        files: [{
+          raw: "Raw data",
+          to: "stable"
+        }]
+      }).to_json)
+
+      processor = CommitRequestProcessor.new 
+      processor.on_message(@options.merge({
+        branch: "master",
+        files: [{
+          raw: "Raw data",
+          to: "master"
+        }]
+      }).to_json)
+
+      content_for("master").match(%r{A  master})
+      content_for("stable").match(%r{A  stable})
     end
   end
 
@@ -63,8 +90,8 @@ describe CommitRequestProcessor do
     it "should move file in repo" do
       add_file("old", "content")
       processor.on_message(@options.to_json)
-      content.match(%r{D  old})
-      content.match(%r{A  new})
+      content_for.match(%r{D  old})
+      content_for.match(%r{A  new})
     end
 
     it "can handle folders" do
@@ -76,8 +103,8 @@ describe CommitRequestProcessor do
         }]
       }
       processor.on_message(@options.merge(hash).to_json)
-      content.match(%r{D\s+file})
-      content.match(%r{A\s+d/e/f})
+      content_for.match(%r{D\s+file})
+      content_for.match(%r{A\s+d/e/f})
     end
   end
 end
