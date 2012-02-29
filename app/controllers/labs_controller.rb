508dbc223bd8df9362b892a79bff60f770acc11a
Jesper Josefsson <jesper.josefsson@gmail.com>
2012-02-29 13:50:23 +0100

    Add method stubs to lab controller

--- app/controllers/labs_controller.rb
+++ app/controllers/labs_controller.rb
@@ -3,10 +3,11 @@ class LabsController < ApplicationController
   def index
     @group = LabGroup.find(params[:group_id])
     @labs = Lab.find_by_group(params[:lab_id])
-    
   end
-
+  # /lab_groups/:group_id/labs/1
   def show
+    @lab = Lab.find(params[:lab_id])
+    @submissions = @lab.submissions_for_group(params[:group_id])
   end
   
   def edit
