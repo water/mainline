6535fd610ee2b2b01c6db3be0cc9537c1c30bc7d
Jesper Josefsson <jesper.josefsson@gmail.com>
2012-02-29 13:32:14 +0100

    Add method stup for find_lab_by_group

--- app/models/lab.rb
+++ app/models/lab.rb
@@ -1,4 +1,9 @@
 class Lab < ActiveRecord::Base
   belongs_to :given_course
   belongs_to :lab_description
+  
+  # Returns all labs for given group
+  def self.find_by_group(group_íd)
+  end
+  
 end
