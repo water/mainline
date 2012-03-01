3c6594ef6a30c02377eb6ecb02af709b41e52c22
Jesper Josefsson <jesper.josefsson@gmail.com>
2012-03-01 10:24:40 +0100

    Add additional user to seed script

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,12 +15,33 @@ user.login = attributes[:login]
 user.save!
 puts attributes.to_yaml.green
 
-course = Course.new()
-# Because of strange validation behaviour
-course.save(:validation => false)
+more_attributes = {
+  login: "examiner",  
+  email: "examiner@popfizzle.com",  
+  password: "abc123",  
+  password_confirmation: "abc123",  
+  terms_of_use: "1", 
+  activated_at: Time.now, 
+  is_admin: true
+}
 
-course_code = CourseCode.new({code: "TDA289"})
-course_code.course = course
-course_code.save!
-puts course_code.to_yaml.green
+user2 = User.new(more_attributes)
+user2.login = more_attributes[:login]
+user.save!
+
+student = Student.create(user: user)
+examiner = Examiner.create(user: user2)
+
+###################
+# Courses and codes
+codes = ["TDA289", "DAT255", "FFR101", "EDA343", "DAT036", "EDA331", "EDA451"]
+codes.each do |code|
+  course_code = CourseCode.new({code: code})
+  course = Course.new
+  course.save!(validation: false)
+  course_code.course = course
+  course_code.save!
+end
+
+
 
