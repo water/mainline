523d80ab79f19428158f39038e43add1d3c2011f
Jesper Josefsson <jesper.josefsson@gmail.com>
2012-03-01 16:15:47 +0100

    Add method stub

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,3 +1,8 @@
 class Submission < ActiveRecord::Base
   belongs_to :lab_has_group
+  
+  # Returns the repo associated with the current submission
+  def repo
+    return Repository.all.first
+  end
 end
