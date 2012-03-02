8c3782d7e787dfe47dcb456eeae220e2bb94953b
Arash Rouhani <miffoljud@gmail.com>
2012-03-02 13:13:24 +0100

    Temporary fix for deactivating Mailer, #50

--- app/models/user_observer.rb
+++ app/models/user_observer.rb
@@ -2,10 +2,12 @@
 
 class UserObserver < ActiveRecord::Observer
   def after_create(user)
+    return # TODO: Temporary fix
     Mailer.signup_notification(user).deliver if user.identity_url.blank?
   end
 
   def after_save(user)
+    return # TODO: Temporary fix
     Mailer.activation(user).deliver if user.recently_activated?  
   end
 end
