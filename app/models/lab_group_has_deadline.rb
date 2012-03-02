3239e19fc3a4f67c7a77ec324192cafa7f051f2a
Arash Rouhani <miffoljud@gmail.com>
2012-03-02 11:22:54 +0100

    Revert "Create and update models for revised database"
    This reverts commit 0c088b9a6d82e7ba7dbc5ffef175d05291467d86.
    
    Conflicts:
    
    	app/models/submission.rb
    
    @anjonas, please run bundle exec to certify that at least the syntax
    is correct, your commit had unbalanced parenthesis and a lot other errors

--- app/models/lab_group_has_deadline.rb
+++
@@ -1,6 +0,0 @@
-class LabGroupHasDeadline
-  belongs_to :lab_has_group_primary, :class_name => 'LabHasGroup', 
-    :foreign_key => 'lab_id' 
-  belongs_to :lab_has_group_secondary, :class_name => 'LabHasGroup', 
-    :foreign_key => 'group_id' 
-end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,14 +1,4 @@
 class Student < ActiveRecord::Base
   belongs_to :base, class_name: "User"
   has_many :registered_courses
-
-  scope :labs_courses_students, joins({given_courses:{registered_courses: :students}})
-
-  #Requires: @lab_num int is a valid lab_id
-  #Ensures: Returns all students registered to the course in which the lab is done
-  def self.students_in_lab(lab_num)
-    a = labs_courses_students.where(labs: {id: lab_num}
-    Student.joins(a:)
-  end
-
 end
