371cea70ce33b2f41b5b1466a90772eccc181c6d
Jesper Josefsson <jesper.josefsson@gmail.com>
2012-03-05 00:37:33 +0100

    Make student belong to user

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,5 @@
 class Student < ActiveRecord::Base
-  belongs_to :base, class_name: "User"
+  belongs_to :user, class_name: "User"
   has_many :registered_courses
   has_and_belongs_to_many :given_courses, join_table: "registered_courses"
   validates_presence_of :base
