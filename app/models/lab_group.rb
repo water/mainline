96fa2360cc4c584577ced0300e0d9915e7c5a18f
Jesper Josefsson <jesper.josefsson@gmail.com>
2012-03-05 14:59:36 +0100

    Add nested attributes for labhasgroups

--- app/models/lab_group.rb
+++ app/models/lab_group.rb
@@ -2,4 +2,5 @@ class LabGroup < ActiveRecord::Base
   has_and_belongs_to_many :registered_courses
   has_many :labs, through: :lab_has_group
   has_many :submissions, through: :lab_has_group
+  accepts_nested_attributes_for :lab_has_groups
 end
