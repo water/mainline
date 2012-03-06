43dd8626b19b9292cc5639d303138c7d1f2965a9
Linus Oleander <linus@oleander.nu>
2012-03-06 16:12:17 +0100

    Create basic specs for LabDescription, all validations passes

--- spec/models/lab_description_spec.rb
+++ spec/models/lab_description_spec.rb
@@ -1,9 +1,29 @@
 describe LabDescription do
-  it "defaults to true" do
-    create(:lab_description).should be_valid
+  describe "validations" do
+    it "defaults to valid" do
+      create(:lab_description).should be_valid
+    end
+
+    it "must have a valid commit hash" do
+      build(:lab_description, commit_hash: "not valid").should_not be_valid
+    end
+
+    it "must have title with length >= 2" do
+      build(:lab_description, title: "a").should_not be_valid
+    end
+
+    it "must have description with length > 5" do
+      build(:lab_description, description: "abcd").should_not be_valid
+    end
+
+    it "must have a 'when' relation" do
+      build(:lab_description, when: nil).should_not be_valid
+    end
   end
 
-  it "should have a 'when'" do
-    create(:lab_description).when.should_not be_nil
+  describe "realtions" do
+    it "should have a 'when'" do
+      create(:lab_description).when.should_not be_nil
+    end
   end
 end
