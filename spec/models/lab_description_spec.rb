eb81f2389a280796d770cb5b0a787aaf4872b93f
Linus Oleander <linus@oleander.nu>
2012-03-06 16:00:23 +0100

    A LabDescription should have a 'when'

--- spec/models/lab_description_spec.rb
+++ spec/models/lab_description_spec.rb
@@ -2,4 +2,8 @@ describe LabDescription do
   it "defaults to true" do
     create(:lab_description).should be_valid
   end
+
+  it "should have a 'when'" do
+    create(:lab_description).when.should_not be_nil
+  end
 end
