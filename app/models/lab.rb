47ca2674e1d033e047bd9fd5c46cec82539078b7
Jonas Ängeslevä <anjonas@student.chalmers.se>
2012-03-06 17:46:15 +0100

    Add validation and test for numericality of :number

--- app/models/lab.rb
+++ app/models/lab.rb
@@ -1,6 +1,7 @@
 class Lab < ActiveRecord::Base
   belongs_to :given_course
-  validates_presence_of :lab_description, :given_course
+  validates_presence_of :lab_description, :given_course, :number
+  validates_numericality_of :number
   #validates_uniqueness_of :lab_description, :scope => :given_course
 
   belongs_to :lab_description, foreign_key: "description_id"
