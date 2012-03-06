61a444d20e96c3fef5f4b234bc73eb3179bdcec9
Linus Oleander <linus@oleander.nu>
2012-03-06 20:01:37 +0100

    LabGroup#identification should increment for each create

--- spec/models/lab_group_spec.rb
+++ spec/models/lab_group_spec.rb
@@ -10,5 +10,9 @@ describe LabGroup do
     it "requires an identification number" do
       build(:lab_group, identification: nil).should_not be_valid
     end
+
+    it "should auto increment the identification number" do
+      3.times.map { create(:lab_group).identification }.should eq([1,2,3])
+    end
   end
 end
