2ba89f8f95849f81d1885882d3093bda146a98db
Linus Oleander <linus@oleander.nu>
2012-03-06 02:39:07 +0100

    Add validation to AssistantRegisteredToGivenCoursesLabHasGroup

--- app/models/lab_has_group.rb
+++ app/models/lab_has_group.rb
@@ -3,6 +3,6 @@ class LabHasGroup < ActiveRecord::Base
   belongs_to :lab_group
   has_one :repository
 
-  has_one :assistant_registered_to_given_course
+  has_one :assistant_registered_to_given_courses_lab_has_group
   has_one :assistant_registered_to_given_course, through: :assistant_registered_to_given_courses_lab_has_group
 end
