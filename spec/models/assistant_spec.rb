bcbadba5808bf274867976573cd6caff42669c70
Jonas Ängeslevä <anjonas@student.chalmers.se>
2012-03-06 21:10:19 +0100

    Add test to assistant_spec

--- spec/models/assistant_spec.rb
+++ spec/models/assistant_spec.rb
@@ -1,5 +1,10 @@
 describe Assistant do
+  let (:assistant)  { Factory.create(:assistant) }
   it "should default to valid" do
-    Factory.create(:assistant).should be_valid
+    Factory.build(:assistant).should be_valid
+  end
+
+  it "should not be valid without a user" do
+    Factory.build(:assistant, user: nil).should_not be_valid
   end
 end
