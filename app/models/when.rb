cf28ee1980da27d492381d454a5e0337084626d7
Linus Oleander <linus@oleander.nu>
2012-03-06 23:08:34 +0100

    Higher span requirement on When#year #82

--- app/models/when.rb
+++ app/models/when.rb
@@ -4,8 +4,8 @@ class When < ActiveRecord::Base
   
   private
   def year_span
-    unless year.to_i.between?(1950, 2050)
-      errors[:year] << "must be between 1950 and 2050"
+    unless year.to_i.between?(1900, 2100)
+      errors[:year] << "must be between 1900 and 2100"
     end
   end
   
