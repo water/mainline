625f0ac2e2951a61f9bf8a0358d5f9eac78aa05c
Jesper Josefsson <jesper.josefsson@gmail.com>
2012-03-07 12:32:01 +0100

    Remove references to project

--- app/helpers/repositories_helper.rb
+++ app/helpers/repositories_helper.rb
@@ -40,8 +40,8 @@ module RepositoriesHelper
     repo_owner_path(@repository, :repository_blob_path, @repository, branch_with_tree(shaish, path), *args)
   end
 
-  def raw_blob_path(shaish, path)
-    repo_owner_path(@repository, :project_repository_raw_blob_path, @project, @repository, branch_with_tree(shaish, path))
+  def raw_blob_path(shaish, path, *args)
+    repo_owner_path(@repository, :repository_raw_blob_path, @repository, branch_with_tree(shaish, path), *args)
   end
 
   def blob_history_path(shaish, path)
