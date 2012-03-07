98b3107f99c678c147dad322ad316d4a2f2b8c89
Linus Oleander <linus@oleander.nu>
2012-03-07 01:00:44 +0100

    Remove files related to #81 and migrate database

--- app/models/registered_course.rb
+++
@@ -1,9 +0,0 @@
-class RegisteredCourse < ActiveRecord::Base
-  belongs_to :student, class_name: "User"
-  belongs_to :given_course
-  has_one :registered_course_has_lab_group
-  has_and_belongs_to_many :lab_groups
-  
-  validates_uniqueness_of :student_id, scope: [:given_course_id]
-  validates_presence_of :student, :given_course
-end
