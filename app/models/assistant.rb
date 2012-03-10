3b9b9dfe07b73d29598da7eb38ba21985d96697d
Linus Oleander <linus@oleander.nu>
2012-03-10 04:41:18 +0100

    Test Assistant#labs

--- app/models/assistant.rb
+++ app/models/assistant.rb
@@ -4,8 +4,8 @@ class Assistant < ActiveRecord::Base
   has_many :given_courses, through: :assistant_registered_to_given_courses
   has_many :lab_has_groups, through: :assistant_registered_to_given_courses
   has_many :lab_groups, through: :lab_has_groups
-
   has_many :all_lab_groups, source: :lab_groups, through: :given_courses
+  has_many :labs, through: :given_courses
 
   validates_presence_of :user
 end
--- spec/models/assistant_spec.rb
+++ spec/models/assistant_spec.rb
@@ -1,23 +1,27 @@
 describe Assistant do
   describe "relations" do
     it "should default to valid" do
-      Factory.build(:assistant).should be_valid
+      build(:assistant).should be_valid
     end
 
     it "should not be valid without a user" do
-      Factory.build(:assistant, user: nil).should_not be_valid
+      build(:assistant, user: nil).should_not be_valid
     end
 
     it "should have a list of given courses" do
-      Factory.create(:assistant).should have_at_least(1).given_courses
+      create(:assistant).should have_at_least(1).given_courses
     end
 
     it "should have a list of lab groups" do
-      Factory.create(:assistant).should have_at_least(1).lab_groups
+      create(:assistant).should have_at_least(1).lab_groups
     end
 
     it "should be able to list all lab groups" do
-      Factory.create(:assistant).should have_at_least(1).all_lab_groups
+      create(:assistant).should have_at_least(1).all_lab_groups
+    end
+
+    it "should have a list of labs" do
+      create(:assistant, labs: [create(:lab)]).should have(1).labs
     end
   end
 end
