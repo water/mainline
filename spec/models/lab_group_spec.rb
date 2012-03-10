9c0987de7466d9ac35452f32f62107426c39437b
Jonas Ängeslevä <anjonas@student.chalmers.se>
2012-03-10 20:02:38 +0100

    Change name of identification column in lab_groups to number

--- spec/models/lab_group_spec.rb
+++ spec/models/lab_group_spec.rb
@@ -27,16 +27,16 @@ describe LabGroup do
       build(:lab_group).should be_valid
     end
 
-    it "should not be possible to override identification number" do
-      create(:lab_group, identification: nil).identification.should_not be_nil
+    it "should not be possible to override number" do
+      create(:lab_group, number: nil).number.should_not be_nil
     end
 
-    it "should auto increment the identification number" do
+    it "should auto increment the number" do
       gc1 = create(:given_course)
       gc2 = create(:given_course)
 
-      list1 = 3.times.map { create(:lab_group, given_course: gc1).identification }
-      list2 = 3.times.map { create(:lab_group, given_course: gc2).identification }
+      list1 = 3.times.map { create(:lab_group, given_course: gc1).number }
+      list2 = 3.times.map { create(:lab_group, given_course: gc2).number }
 
       list1.uniq.count.should eq(3)
       list2.uniq.count.should eq(3)
