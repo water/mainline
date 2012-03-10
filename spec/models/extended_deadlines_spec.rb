e16f6d081d2e13bf05e0987f2e938d94f50fe114
Linus Oleander <linus@oleander.nu>
2012-03-10 17:09:15 +0100

    Test ExtendedDeadline#lab and ExtendedDeadline#lab_group

--- spec/models/extended_deadlines_spec.rb
+++ spec/models/extended_deadlines_spec.rb
@@ -7,5 +7,23 @@ describe ExtendedDeadline do
     it "should have a #at > current time" do
       build(:extended_deadline, at: 3.days.ago).should_not be_valid
     end
+
+    it "should have a lab" do
+      build(:extended_deadline, lab: nil).should_not be_valid
+    end
+
+    it "should have a lab group" do
+      build(:extended_deadline, lab_group: nil).should_not be_valid
+    end
+  end
+
+  describe "relations" do
+    it "belongs to a lab" do
+      create(:extended_deadline).lab.should_not be_nil
+    end
+
+    it "belongs to a lab group" do
+      create(:extended_deadline).lab_group.should_not be_nil
+    end
   end
 end
