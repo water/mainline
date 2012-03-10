ba6c474a8d0c5f9bcc7b579aef23c668a1b2e787
Linus Oleander <linus@oleander.nu>
2012-03-10 18:36:24 +0100

    Test minimum time between each default deadline

--- app/models/lab_default_deadline.rb
+++ app/models/lab_default_deadline.rb
@@ -4,8 +4,20 @@ class LabDefaultDeadline < ActiveRecord::Base
   validates_presence_of :lab, :at
   #validates_uniqueness_of :lab
 
+  validate :time_difference
+
   # What's the minimum time between 
   # each deadline for a given group?
   MINIMUM_TIME_DIFFERENCE = 1.day
-  include DeadlineTimeSpanValidation
+
+private
+  def time_difference
+    in_valid = LabDefaultDeadline.where(lab_id: lab_id).any? do |d| 
+      (d.at.to_i - self.at.to_i) < MINIMUM_TIME_DIFFERENCE.to_i
+    end
+
+    if in_valid
+      errors[:at] << "a bit to similar to some another deadline"
+    end
+  end
 end
--- spec/models/lab_default_deadline_spec.rb
+++ spec/models/lab_default_deadline_spec.rb
@@ -4,7 +4,7 @@ describe LabDefaultDeadline do
   end
 
   it "should not be valid without a :at" do
-    Factory.build(:lab_default_deadline, at: nil ).should_not be_valid
+    Factory.build(:lab_default_deadline, at: nil).should_not be_valid
   end
 
   it "should not be valid without a :lab" do
@@ -15,4 +15,11 @@ describe LabDefaultDeadline do
     Factory.build(:lab_default_deadline, description: nil).should be_valid
   end
 
+  it "should not be have two similar deadlines" do
+    lab1 = create(:lab)
+    lab2 = create(:lab)
+    create(:lab_default_deadline, at: 3.days.from_now, lab: lab1)
+    build(:lab_default_deadline, at: 3.days.from_now + 1.minute, lab: lab1).should_not be_valid
+    build(:lab_default_deadline, at: 3.days.from_now + 1.minute, lab: lab2).should be_valid
+  end
 end
