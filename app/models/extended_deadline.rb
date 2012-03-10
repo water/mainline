4e5ce15c06a9d1896f4098e4e08d89867eaa7bdb
Linus Oleander <linus@oleander.nu>
2012-03-10 17:38:20 +0100

    Test minimum time between each extended deadline

--- app/models/extended_deadline.rb
+++ app/models/extended_deadline.rb
@@ -3,12 +3,26 @@ class ExtendedDeadline < ActiveRecord::Base
   belongs_to :lab
 
   validates_presence_of :lab, :lab_group, :at
-  validate :time_span
+  validate :time_span, :time_difference
+
+  # What's the minimum time between 
+  # each deadline for a given group?
+  MINIMUM_TIME_DIFFERENCE = 1.day
 
 private
   def time_span
     if at and at < Time.zone.now
       errors[:at] << "must be a future value"
+    end
+  end
+
+  def time_difference
+    in_valid = ExtendedDeadline.where(group_id: group_id).any? do |d| 
+      (d.at.to_i - self.at.to_i) < MINIMUM_TIME_DIFFERENCE.to_i
+    end
+
+    if in_valid
+      errors[:at] << "a bit to similar to another deadline"
     end
   end
 end
--- spec/models/extended_deadlines_spec.rb
+++ spec/models/extended_deadlines_spec.rb
@@ -15,6 +15,14 @@ describe ExtendedDeadline do
     it "should have a lab group" do
       build(:extended_deadline, lab_group: nil).should_not be_valid
     end
+
+    it "should not be have two similar deadlines" do
+      group1 = create(:lab_group)
+      group2 = create(:lab_group)
+      create(:extended_deadline, at: 3.days.from_now, lab_group: group1)
+      build(:extended_deadline, at: 3.days.from_now + 1.minute, lab_group: group1).should_not be_valid
+      build(:extended_deadline, at: 3.days.from_now + 1.minute, lab_group: group2).should be_valid
+    end
   end
 
   describe "relations" do
