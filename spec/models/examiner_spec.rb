53e414efb6dc070859b137c1ee6db060f1f0221a
Linus Oleander <linus@oleander.nu>
2012-03-10 05:24:11 +0100

    Test Examiner#given_courses

--- spec/models/examiner_spec.rb
+++ spec/models/examiner_spec.rb
@@ -1,11 +1,17 @@
 describe Examiner do
-  let(:examiner) { Factory.create(:examiner) }
+  describe "validations" do
+    it "should default to valid" do
+      build(:examiner).should be_valid
+    end
 
-  it "should default to valid" do
-    Factory.build(:examiner).should be_valid
+    it "should not be valid without a user" do
+      build(:examiner, user: nil).should_not be_valid
+    end
   end
 
-  it "should not be valid without a user" do
-    Factory.build(:examiner, user: nil).should_not be_valid
+  describe "relations" do
+    it "should have a list of given courses" do
+      create(:examiner, given_courses: [create(:given_course)]).should have(1).given_courses
+    end
   end
 end
