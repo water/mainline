9fd3fb6c60da0b932f572fe4911b859edf26967f
Linus Oleander <linus@oleander.nu>
2012-03-11 01:33:18 +0100

    Fix spec errors related to the Student model due to factory refactoring

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -7,15 +7,18 @@ describe Student do
     end
 
     it "should have a list of registered courses" do
+      student = create(:student, student_registered_for_courses: [create(:student_registered_for_course)])
       student.should have(1).student_registered_for_courses
     end
 
     it "should have a list of given courses" do
+      student = create(:student, given_courses: [create(:given_course)])
       student.should have(1).given_courses
     end
 
     it "should have a list of lab groups" do
-      create(:lab_group, student_registered_for_courses: student.student_registered_for_courses)
+      group = create(:lab_group)
+      create(:student_registered_for_course, lab_groups: [group], student: student)
       student.should have(1).lab_groups
     end
   end
