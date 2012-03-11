f32e15a5adb6b06cfa70d8bf42fd2e44d8744659
Linus Oleander <linus@oleander.nu>
2012-03-11 23:01:25 +0100

    Add User, Administrator, Examiner, Assistant, Student, Course, LabGroup, Lab and GivenCourse factory to seed script [#67]

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,36 +9,65 @@ if ENV["CLEAR"]
 end
 
 FactoryGirl.reload
-user = Factory.create(:admin, {
+
+#### User
+user = Factory.create(:user, {
   email: "admin@popfizzle.com",
   password: "abc123"
 })
 
-repository = Factory.create(:repository, {
-  user: user, 
-  owner: user,
-  name: "repo1"
+#### Administrator
+Factory.create(:administrator, user: user)
+
+#### Examiner
+examiner = Factory.create(:examiner, user: user)
+
+#### Assistant
+assistant = Factory.create(:assistant, user: user)
+
+#### Student
+student = Factory.create(:student, user: user)
+
+#### Course
+course = Factory.create(:course, {
+  department_attributes: { name: "IT" },
+  course_codes_attributes: [{ code: "TDA123" }, { code: "EDA331" }]
 })
 
-sleep(10) # Wait for repository to be created
-unless repository.full_repository_path
-  abort("You have to start foreman first".red)
-end
+#### LabGroup
+lab_group = Factory.create(:lab_group)
 
-paths = repository.full_repository_path.split("/")
-path = paths[0..-2].join("/")
-git = paths.last
-puts %x{
-  cd #{path} && 
-  rm -rf #{git} &&
-  git clone --bare git://github.com/water/grack.git #{git}
-} unless git =~ /^\//
+#### Lab
+lab1 = Factory.create(:lab, active: true)
+lab2 = Factory.create(:lab, active: false)
 
-# codes = ["TDA289", "DAT255", "FFR101", "EDA343", "DAT036", "EDA331", "EDA451"]
-# codes.each do |code|
-#   course_code = CourseCode.new({code: code})
-#   course = Course.new
-#   course.save!(validation: false)
-#   course_code.course = course
-#   course_code.save!
+#### GivenCourse
+given_course = Factory.create(:given_course, {
+  course: course, 
+  examiners: [examiner],
+  assistants: [assistant],
+  students: [student],
+  labs: [lab1, lab2],
+  lab_groups: [lab_group]
+})
+
+# repository = Factory.create(:repository, {
+#   user: user, 
+#   owner: user,
+#   name: "repo1"
+# })
+
+# sleep(10) # Wait for repository to be created
+# unless repository.full_repository_path
+#   abort("You have to start foreman first".red)
 # end
+# 
+# paths = repository.full_repository_path.split("/")
+# path = paths[0..-2].join("/")
+# git = paths.last
+# puts %x{
+  # cd #{path} && 
+  # rm -rf #{git} &&
+  # git clone --bare git://github.com/water/grack.git #{git}
+#} unless git =~ /^\//
+
