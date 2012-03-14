20a7abbb5d321d5a580e59431c74b0b2075a5f82
Jesper Josefsson <jesper.josefsson@gmail.com>
2012-03-14 22:38:50 +0100

    Add trees controller spec which exposes potential problem in repo factory #102

---
+++ spec/controllers/trees_controller_spec.rb
@@ -0,0 +1,11 @@
+describe TreesController do
+  render_views
+  let(:repository) { Factory.create(:repo_with_data) }
+  
+  describe "GET tree root" do
+    it "displays a table" do
+      visit repository_tree_path(repository, "master", bare: 1)
+      page.should have_selector("table")
+    end
+  end
+end
