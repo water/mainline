d2160b0c930bba283cc0161aae7585243d68aef7
Jesper Josefsson <jesper.josefsson@gmail.com>
2012-03-14 16:44:02 +0100

    Partial move of code from submissions to labs controller

--- app/controllers/labs_controller.rb
+++ app/controllers/labs_controller.rb
@@ -1,5 +1,7 @@
 class LabsController < ApplicationController
   respond_to :html
+  before_filter :find_repo, only: [:upload]
+  before_filter :add_paths_to_gon, only: [:upload]
   
   #
   # GET /labs
@@ -39,5 +41,22 @@ class LabsController < ApplicationController
 
   def create
   end
-
+  
+  def upload
+  end
+  
+  protected
+  def find_repo
+    @repository = Repository.find_by_user_and_lab(current_user, params[:lab_id])
+  end
+  def add_paths_to_gon
+    gon.commit_request_path = repository_commit_requests_path(@repository.id)
+    # TODO Make this nicer!
+    gon.tree_root_path = repository_tree_path(@repository, "master", bare: 1)
+    gon.repository_path = repository_path(@repository)
+    gon.ref = "master"
+    gon.repository_id = @repository.id
+    gon.faye_port = APP_CONFIG["faye"]["port"]
+    gon.user_token = current_user.token
+  end
 end
--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -14,20 +14,4 @@ class SubmissionsController < ApplicationController
 
   def new
   end
-
-  protected
-  def find_repo
-    # @repository = Repository.find_by_group_and_lab(params[:group_id], params[:lab_id])
-    @repository = Repository.find(1)
-  end
-  def add_paths_to_gon
-    gon.commit_request_path = repository_commit_requests_path(@repository.id)
-    # TODO Make this nicer!
-    gon.tree_root_path = repository_tree_path(@repository, "master", bare: 1)
-    gon.repository_path = repository_path(@repository)
-    gon.ref = "master"
-    gon.repository_id = @repository.id
-    gon.faye_port = APP_CONFIG["faye"]["port"]
-    gon.user_token = current_user.token
-  end
 end
