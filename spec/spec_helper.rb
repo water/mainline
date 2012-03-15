320fe2e36f78e1c27ea1216198065c2c6c100671
Linus Oleander <linus@oleander.nu>
2012-03-15 02:32:25 +0100

    Add cucumber features

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,6 +12,7 @@ Spork.prefork do
   require File.expand_path("../../config/environment", __FILE__)
   require "rspec/rails"
   require "rspec/autorun"
+  require "cucumber/rails"
   require "capybara/rails"
   require "database_cleaner"
   
