8fc07c8ae59d27c439954383790c56bfe887ce40
Linus Oleander <linus@oleander.nu>
2012-03-15 19:12:17 +0100

    Implement failing test for BlobsController

--- spec/controllers/blobs_controller_spec.rb
+++ spec/controllers/blobs_controller_spec.rb
@@ -6,6 +6,7 @@ describe BlobsController do
   describe "GET blob root" do
     it "doesn't crash" do
       visit repository_blob_path(repository, "master/Rakefiler", bare: 1)
+      page.status_code.should eq(200)
     end
   end
 end
