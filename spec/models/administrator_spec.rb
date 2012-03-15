bfc24ba93a23cece2ad272190d59b974d1aa1731
Linus Oleander <linus@oleander.nu>
2012-03-15 19:37:01 +0100

    Test and implement Administrator#labs

--- spec/models/administrator_spec.rb
+++ spec/models/administrator_spec.rb
@@ -8,4 +8,12 @@ describe Administrator do
       build(:administrator, user: nil).should_not be_valid
     end
   end
+
+  describe "relations" do
+    it "should have a list of labs" do
+      create(:lab, active: false)
+      create(:lab, active: true)
+      create(:administrator).should have(2).labs
+    end
+  end
 end
