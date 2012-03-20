f7f83fbe150a0269720777156a29a1f618b4306e
Jesper Josefsson <jesper.josefsson@gmail.com>
2012-03-20 10:23:50 +0100

    Methods for checking whether a student is registered for a course

--- app/models/student.rb
+++ app/models/student.rb
@@ -5,4 +5,12 @@ class Student < ActiveRecord::Base
   has_many :labs, through: :lab_groups
   has_and_belongs_to_many :given_courses, join_table: "student_registered_for_courses"
   validates_presence_of :user
+  
+  #
+  # Checks whether a student is registered for a given course.
+  #
+  def registered_for_course?(given_course)
+    StudentRegisteredForCourse.reg_for_student_and_course.exists?
+  end
+  
 end
