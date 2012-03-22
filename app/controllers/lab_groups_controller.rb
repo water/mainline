eac2671dfc40d1382aa4ade8f5ec753413e072da
Jesper Josefsson <jesper.josefsson@gmail.com>
2012-03-22 15:39:08 +0100

    Add rudimentary views for lab_groups_controller#show

--- app/controllers/lab_groups_controller.rb
+++ app/controllers/lab_groups_controller.rb
@@ -1,8 +1,12 @@
 class LabGroupsController < ApplicationController
+  respond_to :html
   def index
   end
 
   def show
+    @lab_group = LabGroup.find(params[:id])
+    @labs = @lab_group.labs
+    respond_with(@lab_group)
   end
 
   def new
