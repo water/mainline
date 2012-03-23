092f6f7963c8bd51bbecfa35d5cd6a494aa5f4cb
Arash Rouhani <miffoljud@gmail.com>
2012-03-23 18:22:18 +0100

    Fetch latest commit in Submission, close #123

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -7,13 +7,25 @@ class Submission < ActiveRecord::Base
   validates_format_of :commit_hash, with: /^[a-f0-9]{40}$/
   validates_presence_of :commit_hash, :lab_has_group
   validate :lab_access
+  validate :commit_hash_exists
+
+  before_validation :fetch_commit, :if=>lambda {|a| a.commit_hash.nil?}
 
   alias_method :repo, :repository
+
+  def fetch_commit
+    @temp = lab_has_group.repository.head_candidate.commit
+    define_singleton_method(:commit_hash) { @temp } # FIXME: fulhack :)
+  end
 
   private
     def lab_access
       if lab and not lab.active?
         errors[:lab_access]  << "failed, lab not accessible"
       end
+    end
+
+    def commit_hash_exists
+      # TODO: define
     end
 end
