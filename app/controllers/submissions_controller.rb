a6954d8b668c27664ac691060f2e706447d41834
Jesper Josefsson <jesper.josefsson@gmail.com>
2012-03-24 17:30:19 +0100

    Naive solution for submission creation

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -10,7 +10,16 @@ class SubmissionsController < ApplicationController
   def create
     lab_group = LabGroup.find(params[:lab_group_id])
     lhg = lab_group.lab_has_groups.where(lab_id: params[:lab_id]).first
-    Submission.create(lab_has_group: lhg)
+    if Submission.create(lab_has_group: lhg)
+      flash[:notice] = "Submission successful"
+    else
+      flash[:error] = "Submissions failed"
+    end
+    redirect_to course_lab_group_lab_path(
+      current_role_name, 
+      params[:course_id], 
+      params[:lab_group_id], 
+      params[:lab_id])
   end
 
   def new
