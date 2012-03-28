3ba5461a241cd7e1b0df68976b89e0c23b4a214a
Linus Oleander <linus@oleander.nu>
2012-03-28 02:26:13 +0200

    LabGroup should create a link between it self and Lab

--- app/models/lab_group.rb
+++ app/models/lab_group.rb
@@ -1,14 +1,34 @@
 class LabGroup < ActiveRecord::Base
   has_and_belongs_to_many :student_registered_for_courses
+
   has_many :lab_has_groups
   has_many :labs, through: :lab_has_groups
   has_many :submissions, through: :lab_has_groups
+  has_many :students, through: :student_registered_for_courses
+
   belongs_to :given_course
-  has_many :students, through: :student_registered_for_courses, class_name: "Student"
 
   acts_as_list scope: :given_course, column: :number
   accepts_nested_attributes_for :lab_has_groups
   
+  #
+  # Creates a link between Lab 
+  # and LabGroup using LabHasGroup
+  #
+  after_create do |group|
+    labs = group.lab_has_groups.
+      includes(:lab).map(&:lab)
+
+    group.given_course.labs.each do |lab|
+      next if labs.include?(lab)
+      LabHasGroup.create!({
+        lab: lab,
+        lab_group: group,
+        repository: Repository.create!
+      })
+    end
+  end
+
   #
   # Adds a student to a lab group.
   # Checks that the student is registered to the correct course.
