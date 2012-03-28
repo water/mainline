8068bd512e2cc2fdd04b73effbeb42d30f1cb438
Sofia Larsson <karinsofiapaulina@gmail.com>
2012-03-28 18:12:58 +0200

    Fix test for join

--- spec/controllers/lab_groups_controller_spec.rb
+++ spec/controllers/lab_groups_controller_spec.rb
@@ -2,19 +2,17 @@ describe LabGroupsController do
   describe "join" do
     it "should allow students to join lab_groups" do
       student1 = Factory.create(:student)
-      student2 = Factory.create(:student)
 
       gc = Factory.create(:given_course)
       lg = Factory.create(:lab_group, given_course: gc)
 
       src1 = Factory.create(:student_registered_for_course, given_course: gc, student: student1)
-      src2 = Factory.create(:student_registered_for_course, given_course: gc, student: student2)
 
+      login_as(student1)
       lg.student_registered_for_courses << src1
-      put :join,  course_id: gc, id: lg, user_id: student2
+      post :join,  role: "student", given_course: gc, lab_group: {id: lg.id}
 
-
-      lg.student_registered_for_courses.exists?(src2).should be_true
+      lg.student_registered_for_courses.exists?(src1).should be_true
     end
   end
 end
