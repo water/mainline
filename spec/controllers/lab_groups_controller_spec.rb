219c5ee237cfcae137e38c4d16dcc5b921a6a9b1
Sofia Larsson <karinsofiapaulina@gmail.com>
2012-03-29 22:04:24 +0200

    Remove not member test

--- spec/controllers/lab_groups_controller_spec.rb
+++ spec/controllers/lab_groups_controller_spec.rb
@@ -1,27 +1,17 @@
 describe LabGroupsController do
   describe "join" do
-    student1 = Factory.create(:student)
-    student2 = Factory.create(:student)
+    student = Factory.create(:student)
     gc = Factory.create(:given_course)
     lg = Factory.create(:lab_group, given_course: gc)
-    src1 = Factory.create(:student_registered_for_course, 
-      given_course: gc, student: student1)
-    src2 = Factory.create(:student_registered_for_course, 
-      given_course: gc, student: student2)
-
-    it "should not be member of group" do
-      login_as(student1)
-      post :join, role: "student", given_course: gc, 
-        lab_group: {id: lg.id}
-      lg.student_registered_for_courses.exists?(src1).should be_false
-    end
+    src = Factory.create(:student_registered_for_course, 
+      given_course: gc, student: student)
 
     it "should be member of group" do
-      login_as(student2)
+      login_as(student)
       post :join, role: "student", given_course: gc, 
         lab_group: {id: lg.id}
-      lg.student_registered_for_courses << src2
-      lg.student_registered_for_courses.exists?(src2).should be_true
+      lg.student_registered_for_courses << src
+      lg.student_registered_for_courses.exists?(src).should be_true
     end
 
   end
