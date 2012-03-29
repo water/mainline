139da9d745fd0e7544dc27a2b0b5edc22a24bc75
Sofia Larsson <karinsofiapaulina@gmail.com>
2012-03-29 18:58:30 +0200

    Restructure join test

--- spec/controllers/lab_groups_controller_spec.rb
+++ spec/controllers/lab_groups_controller_spec.rb
@@ -4,19 +4,26 @@ describe LabGroupsController do
     student2 = Factory.create(:student)
     gc = Factory.create(:given_course)
     lg = Factory.create(:lab_group, given_course: gc)
-    src1 = Factory.create(:student_registered_for_course, given_course: gc, student: student1)
-    src2 = Factory.create(:student_registered_for_course, given_course: gc, student: student2)
-    lg.student_registered_for_courses << src2
+    src1 = Factory.create(:student_registered_for_course, 
+      given_course: gc, student: student1)
+    src2 = Factory.create(:student_registered_for_course, 
+      given_course: gc, student: student2)
+
     it "should not be member of group" do
       login_as(student1)
-      post :join, role: "student", given_course: gc, lab_group: {id: lg.id}
+      post :join, role: "student", given_course: gc, 
+        lab_group: {id: lg.id}
       lg.student_registered_for_courses.exists?(src1).should be_false
     end
+
     it "should be member of group" do
       login_as(student2)
-      post :join, role: "student", given_course: gc, lab_group: {id: lg.id}
+      post :join, role: "student", given_course: gc, 
+        lab_group: {id: lg.id}
+      lg.student_registered_for_courses << src2
       lg.student_registered_for_courses.exists?(src2).should be_true
     end
+
   end
   describe "POST /create" do
     student = Factory.create(:student)
