92688812661f3f4f1f4d0d7e339744b69d537fe7
Arash Rouhani <miffoljud@gmail.com>
2012-04-23 17:42:26 +0200

    Began on an git post-receive hook [#143]

---
+++ data/hooks/post-receive.rb
@@ -0,0 +1,28 @@
+# A bit of ruby comments
+
+p Dir.pwd
+oldrev, newrev, refname = gets.split
+p [oldrev, newrev, refname]
+
+if refname.split("/").last != "master"
+  puts "You did not push to master, so water doesn't process your commits"
+  exit 0
+end
+
+commits = `git rev-list #{oldrev}..#{newrev}`.split("\n")
+submit_hash = nil
+commits.each { |commit_hash|
+  msg = `git show --format=format:"%s" #{commit_hash}`.split("\n").first
+  # puts "I know #{commit_hash} has message #{msg}"
+  submit_hash ||= commit_hash if msg.include? "#submit"
+}
+
+unless submit_hash
+  puts "We see that you didn't want to submit your code, so we don't"
+  exit 0
+end
+
+# TODO: Implement
+puts "Ok, water tries to submit #{submit_hash} for you ..."
+
+puts "Submission recieved sucessfully ... NOT!"
