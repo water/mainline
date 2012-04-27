adbc288161da0b53639b3be9a61e03a5ecc85680
Jonas Ängeslevä <anjonas@student.chalmers.se>
2012-04-27 15:56:23 +0200

    Remove notes method from reviews_controller and fix bug in grade#reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,19 +1,9 @@
 class ReviewsController < ApplicationController
 
-  def notes
-    p params
-    p Submission.find(params[:id]).notes
-    if current_role.is_a?(Assistant) or current_role.is_a?(Examiner)
-      puts "Starting"
-      Submission.find(params[:id]).update_attribute!(:notes, params[:notes]) 
-    end
-    redirect_to :controller => 'submissions', :action => 'show'
-  end
-
   def grade
 	  if current_role.is_a?(Assistant) or current_role.is_a?(Examiner)
 	    @submission = Submission.find(params[:id])
-	    @submission.lab_has_group.grade = params[:grade]
+	    @submission.lab_has_group.update_attribute(:grade,params[:grade])
 	  end
    redirect_to :controller => 'labs', :action => 'submissions'
  	end
