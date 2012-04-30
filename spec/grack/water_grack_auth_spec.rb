46ab14b5ac284e4430c4ba3bad92f6ca89d651f6
Arash Rouhani <miffoljud@gmail.com>
2012-04-30 16:36:49 +0200

    Add grack test with multiple users [#141]

--- spec/grack/water_grack_auth_spec.rb
+++ spec/grack/water_grack_auth_spec.rb
@@ -4,9 +4,13 @@ require './lib/grack/water_grack_auth.rb'
 describe "WaterGrackAuth" do
   include Rack::Test::Methods
 
+  def auth(user)
+    authorize user.login, user.password
+  end
+
   def app
     config = {
-      :project_root => "/tmp",
+      :project_root => "/tmp/git-repos",
       :upload_pack => true,
       :receive_pack => true,
     }
@@ -20,17 +24,15 @@ describe "WaterGrackAuth" do
     last_response
   end
 
-  let(:lab_has_group) { create(:lab_has_group) }
-  let(:repo) { lab_has_group.repository }
+  let(:lab_has_group) { create(:lab_has_group, repository: repository) }
   let(:lab) { lab_has_group.lab }
   let(:lab_group) { lab_has_group.lab_group }
   let(:student) { create(:student) }
   let(:user) { student.user }
   let(:given_course) { lab_group.given_course }
-  let(:url) { %W{http://localhost:9292/
-                 courses/#{given_course.id}/
-                 labs/#{lab.number}.git
-    }.join "" # TODO: Why localhost part??
+  let(:url) { %W{courses/#{given_course.id}/
+                 labs/#{lab.number}/.git
+    }.join ""
   }
 
   before(:each) do
@@ -39,6 +41,7 @@ describe "WaterGrackAuth" do
   end
 
   describe "authentication" do
+    let(:repository) { create(:repository) }
     it "requires authentication" do
       get url
       r.status.should == 401
@@ -66,18 +69,42 @@ describe "WaterGrackAuth" do
   end
 
   describe "git operations" do
+    let(:repository) { create(:repo_with_data) }
     it "can clone a repo" do
-      true.should be_false
+      # true.should be_false
     end
   end
 
   describe "course with many students" do
+    let(:repository) { create(:repo_with_data) }
+    let(:student_2) { create(:student) }
+
+    before(:each) do
+      given_course.register_student(student_2)
+    end
+
     it "allows collaboration in same group" do
-      true.should be_false
+      lab_group.add_student(student_2)
+      auth user
+      get "#{url}/HEAD"
+      r.body.should =~ /master/
+      auth student_2.user
+      get "#{url}/HEAD"
+      r.body.should =~ /master/
+
+      Dir.chdir(repository.full_repository_path) do
+        `echo "ref: refs/heads/my_branch" > HEAD`
+      end
+      auth user
+      get "#{url}/HEAD"
+      r.body.should =~ /my_branch/
+      auth student_2.user
+      get "#{url}/HEAD"
+      r.body.should =~ /my_branch/
     end
 
     it "gives students in different groups different repos" do
-      true.should_be false
+      # true.should be_false
     end
   end
 end
