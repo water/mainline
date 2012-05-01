43282b2df2d79a2545c60575fca40de37a9f789c
Jonas Ängeslevä <anjonas@student.chalmers.se>
2012-05-01 22:08:25 +0200

    Add cancan authorization to reviews_controller methods

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,7 +5,9 @@ class ReviewsController < ApplicationController
   def grade
 	  if current_role.is_a?(Assistant) or current_role.is_a?(Examiner)
 	    @submission = Submission.find(params[:id])
-	    @submission.lab_has_group.update_attribute(:grade,params[:grade])
+      if can?(:grade, @submission)
+	      @submission.lab_has_group.update_attribute(:grade,params[:grade])
+      end
 	  end
     redirect_to :controller => 'labs', :action => 'submissions'
  	end
@@ -13,17 +15,19 @@ class ReviewsController < ApplicationController
   def state
     if current_role.is_a?(Assistant) or current_role.is_a?(Examiner)
       @submission = Submission.find(params[:id])
-      lhg = @submission.lab_has_group
-      if(params[:state] == 'accepted')
-        lhg.accepted!
-      elsif(params[:state] == 'reviewing')
-        lhg.reviewing!
-      elsif(params[:state] == 'rejected')
-        lhg.rejected!
-      elsif(params[:state] == 'pending')
-        lhg.pending!
+      if can?(:state, @submission)
+        lhg = @submission.lab_has_group
+        if(params[:state] == 'accepted')
+          lhg.accepted!
+        elsif(params[:state] == 'reviewing')
+          lhg.reviewing!
+        elsif(params[:state] == 'rejected')
+          lhg.rejected!
+        elsif(params[:state] == 'pending')
+          lhg.pending!
+        end
       end
-    end
+    end  
     respond_with(@submission)
   end
 end
