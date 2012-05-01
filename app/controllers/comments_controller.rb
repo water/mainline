c54acd89e7c2a0db438aa42cf82404b769210398
Jonas Ängeslevä <anjonas@student.chalmers.se>
2012-05-01 22:11:28 +0200

    Add crud method skeletons to comments_controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,4 +4,16 @@ class CommentsController < ApplicationController
       @comment = Comment.new(:parent_id => params[:parent_id])
   end
 
+  def create
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+
 end
