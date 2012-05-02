d3725530b1aab9baa164ac4787d4bfb1e6018b1b
Jonas Ängeslevä <anjonas@student.chalmers.se>
2012-05-02 13:01:12 +0200

    Add methods edit, show, destroy adn create to comments_controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,15 +5,37 @@ class CommentsController < ApplicationController
   end
 
   def create
+    @comment = comment.new(params[:message])
+    if @message.save
+      flash.notice = "Comment created"
+    end
+
+    redirect_to :back
+    rescue ApplicationController::RedirectBackError
+      redirect_to root_url
+    end
   end
 
   def edit
+    @comment = Comment.find(params[:id])
+
+    if @comment.update_attributes(params[:body])
+      flash.notice = "You updated the comment"
+    end
+    respond_with(@comment)
   end
 
   def show
+    @comment = comment.find(params[:id])
   end
 
   def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    
+    redirect_to :back
+    rescue ApplicationController::RedirectBackError
+      redirect_to root_url
+    end
   end
-
 end
