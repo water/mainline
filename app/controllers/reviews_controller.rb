43bff60a67b1f488800d192a0453d767956cac8c
Jesper Josefsson <jesper.josefsson@gmail.com>
2012-05-03 10:54:30 +0200

    Use new ability verb and remove use of instance vars [#156]

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,23 +4,23 @@ class ReviewsController < ApplicationController
 
 
   def review
-    @submission = Submission.find(params[:id])
+    submission = Submission.find(params[:id])
 
-    if @grade = params[:grade]
-      if can?(:grade, @submission)
-        @submission.lab_has_group.update_attribute(:grade, @grade)
+    if grade = params[:grade]
+      if can?(:review, submission)
+        submission.lab_has_group.update_attribute(:grade, grade)
       end
      redirect_to labs_path, notice: "Grade was changed on submission"
     end
 
-    if @state = params[:state]
-      if can?(:state, @submission)
-        @submission.lab_has_group.send("#{@state}!")
+    if state = params[:state]
+      if can?(:review, submission)
+        submission.lab_has_group.send("#{@state}!")
         redirect_to course_lab_group_lab_submission_path(
           current_role_name, 
           params[:course_id], 
           params[:lab_group_id], 
-          @submission), 
+          submission), 
           notice: "State was changed on submission"
       end
     end
