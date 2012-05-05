e03b5da0aeee0c019680ab2ebfa8365335f9d72b
Jesper Josefsson <jesper.josefsson@gmail.com>
2012-05-05 14:32:32 +0200

    Add lab group relation to artgc [#157]

--- app/models/assistant_registered_to_given_course.rb
+++ app/models/assistant_registered_to_given_course.rb
@@ -3,5 +3,6 @@ class AssistantRegisteredToGivenCourse < ActiveRecord::Base
   belongs_to :assistant
   has_many :assistant_registered_to_given_course_has_lab_has_groups
   has_many :lab_has_groups, through: :assistant_registered_to_given_course_has_lab_has_groups
+  has_many :lab_groups, through: :lab_has_groups
   validates_presence_of :given_course, :assistant
 end
