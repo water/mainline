95b8de7b50d847a2d2374dea49a739f90311b512
Jesper Josefsson <jesper.josefsson@gmail.com>
2012-05-05 13:00:39 +0200

    Amend assistant to lhg relation [#157]

--- app/models/assistant_registered_to_given_course.rb
+++ app/models/assistant_registered_to_given_course.rb
@@ -2,6 +2,6 @@ class AssistantRegisteredToGivenCourse < ActiveRecord::Base
   belongs_to :given_course
   belongs_to :assistant
   has_one :assistant_registered_to_given_courses_lab_has_group
-  has_one :lab_has_group, through: :assistant_registered_to_given_courses_lab_has_group
+  has_many :lab_has_groups, through: :assistant_registered_to_given_courses_lab_has_group
   validates_presence_of :given_course, :assistant
 end
--- app/models/assistant_registered_to_given_courses_lab_has_group.rb
+++ app/models/assistant_registered_to_given_courses_lab_has_group.rb
@@ -1,5 +1,5 @@
 class AssistantRegisteredToGivenCoursesLabHasGroup < ActiveRecord::Base
-  belongs_to :lab_has_group
+  has_many :lab_has_groups
   belongs_to :assistant_registered_to_given_course
   validates_uniqueness_of :lab_has_group_id, scope: [:assistant_registered_to_given_course_id]
   validates_presence_of :lab_has_group, :assistant_registered_to_given_course
