ec2a2d6ed66cc29a3674066d0f8464c5ae863eed
Jonas Ängeslevä <anjonas@student.chalmers.se>
2012-05-08 14:54:10 +0200

    Create registrations_controller

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,32 @@
+# encoding: utf-8
+class RegistrationsController < ApplicationController
+
+	def new
+	end
+
+	def create
+	end
+
+	def register
+		ActiveRecord::Base.transaction do
+			lab_group = LabGroup.create!(
+				given_course_id: params[:course_id],
+				students: [current_user]
+			)
+
+			repository = Repository.create!()
+
+			LabHasGroup.create!(
+				repository: repository, 
+				lab_id: params[:lab_id],
+				lab_group: lab_group
+			)
+	  end
+	  if LabGroup.exists?(lab_group)
+	  	redirect_back notice: "You have joined a lab and lab group"
+	  else
+	  	redirect_back notice: "Something went wrong"
+	  end
+	end
+
+end
