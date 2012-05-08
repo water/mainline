d8c57a4235a7312df32205f97b6054c5b3d47d96
Jesper Josefsson <jesper.josefsson@gmail.com>
2012-05-08 15:46:48 +0200

    Add token methods to lab_group and raise exception in #add_student

--- app/models/lab_group.rb
+++ app/models/lab_group.rb
@@ -6,8 +6,25 @@ class LabGroup < ActiveRecord::Base
   belongs_to :given_course
   has_many :students, through: :student_registered_for_courses, class_name: "Student"
 
+  attr_accessor :hidden_token
+  
   acts_as_list scope: :given_course, column: :number
   accepts_nested_attributes_for :lab_has_groups
+  
+  # @token 40 char SHA1 string
+  # @return User A user
+  #
+  def self.find_by_token(token)
+    self.find_by_sql([
+      "SELECT * FROM lab_groups WHERE ENCODE(DIGEST(? || id, 'sha1'), 'hex') = ? LIMIT 1",
+      APP_CONFIG["salt"], token
+    ]).first
+  end
+  
+  def token
+    LabGroup.find_by_sql(["SELECT id, ENCODE(DIGEST(? || id, 'sha1'), 'hex') FROM lab_groups WHERE id = ?", APP_CONFIG["salt"], self.id.to_s])
+    # Digest::SHA1.hexdigest(APP_CONFIG["salt"] || self.id.to_s)
+  end
   
   #
   # Adds a student to a lab group.
@@ -17,6 +34,9 @@ class LabGroup < ActiveRecord::Base
     @registration = StudentRegisteredForCourse.reg_for_student_and_course(student, self.given_course)
     if @registration
       self.student_registered_for_courses << @registration
+    else
+      raise "Registration failed"
     end
   end
+  
 end
