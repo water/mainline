5553a7acbbe49f35d304715354ce3084fbd3595f
Jesper Josefsson <jesper.josefsson@gmail.com>
2012-05-15 09:53:40 +0200

    Switch back to an unscoped dashboard for root

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # encoding: utf-8
 
 Gitorious::Application.routes.draw do
-  root to: "dashboards#show", role: "student"
+  root to: "dashboards#show"
   resources :registered_courses, :students
   resources :lab_deadlines, :study_periods, :course_codes
 
