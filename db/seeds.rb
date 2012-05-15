6b8ae78e3581f48dd60ebf5fb07c4084477491fd
Arash Rouhani <miffoljud@gmail.com>
2012-05-15 12:23:57 +0200

    Move back old seeds to top of file

--- db/seeds.rb
+++ db/seeds.rb
@@ -56,6 +56,98 @@ initial_lab_commit = FactoryGirl.create(:initial_lab_commit, {
 })
 
 ###################################
+###### Old seeds ##################
+###################################
+
+#### User
+user = FactoryGirl.create(:user, {
+  email: "admin@popfizzle.com",
+  password: "abc123",
+  login: "pelle"
+})
+
+
+#### Administrator
+FactoryGirl.create(:administrator, user: user)
+
+#### Examiner
+examiner = FactoryGirl.create(:examiner, user: user)
+
+#### Assistant
+assistant = FactoryGirl.create(:assistant, user: user)
+
+#### Student
+student = FactoryGirl.create(:student, user: user)
+
+#### Course
+course = FactoryGirl.create(:course, {
+  department_attributes: { name: "IT_2" },
+  course_codes_attributes: [{ code: "TDA123" }, { code: "EDA331" }]
+})
+
+#### GivenCourse
+given_course = FactoryGirl.create(:given_course, {
+  course: course, 
+  examiners: [examiner],
+  assistants: [assistant],
+  students: [student],
+  study_period: study_period
+})
+
+#### Lab
+labs << FactoryGirl.create(:lab, {
+  active: true,
+  initial_lab_commit: initial_lab_commit,
+  lab_description: lab_description,
+  given_course: given_course
+})
+
+labs << FactoryGirl.create(:lab, {
+  active: false,
+  given_course: given_course
+})
+
+#### LabGroup
+lab_group = FactoryGirl.create(:lab_group, {
+  given_course: given_course
+})
+
+lab_group.add_student(student)
+
+#### LabHasGroup
+labs.each_with_index do |lab, index|
+  lhg = FactoryGirl.create(:lab_has_group, {
+    lab: lab, 
+    lab_group: lab_group,
+    repository: FactoryGirl.create(:repo_with_data)
+  })
+
+  FactoryGirl.create(:submission, {
+    commit_hash: commits[index],
+    lab_has_group: lhg
+    #comment: FactoryGirl.create(:comment)
+  })
+end
+
+### DefaultDeadline
+labs.each_with_index do |lab, i|
+  FactoryGirl.create(:default_deadline, {
+    lab: lab,
+    at: ((i + 1) * 2).days.from_now
+  })
+end
+
+#### ExtendedDeadline
+labs.each do |lab|
+  lab.lab_has_groups.each_with_index do |lhg, i|
+    FactoryGirl.create(:extended_deadline, {
+      lab_has_group: lhg,
+      at: ((i + 1) * 5).days.from_now
+    })
+  end
+end
+
+###################################
 ###### Interesting course seed ####
 ###################################
 
@@ -143,100 +235,4 @@ lab_group_1_1_2.add_students students[3,6]
 lab_group_1_1_3.add_students students[0,2]
 lab_group_1_1_4.add_students students[2,4]
 lab_group_1_1_5.add_students students[4,6]
-
-###################################
-###### Old seeds ##################
-###################################
-
-#### User
-user = FactoryGirl.create(:user, {
-  email: "admin@popfizzle.com",
-  password: "abc123",
-  login: "pelle"
-})
-
-
-#### Administrator
-FactoryGirl.create(:administrator, user: user)
-
-#### Examiner
-examiner = FactoryGirl.create(:examiner, user: user)
-
-#### Assistant
-assistant = FactoryGirl.create(:assistant, user: user)
-
-#### Student
-student = FactoryGirl.create(:student, user: user)
-
-#### Course
-course = FactoryGirl.create(:course, {
-  department_attributes: { name: "IT_2" },
-  course_codes_attributes: [{ code: "TDA123" }, { code: "EDA331" }]
-})
-
-#### GivenCourse
-given_course = FactoryGirl.create(:given_course, {
-  course: course, 
-  examiners: [examiner],
-  assistants: [assistant],
-  students: [student],
-  study_period: study_period
-})
-
-#### Lab
-labs << FactoryGirl.create(:lab, {
-  active: true,
-  initial_lab_commit: initial_lab_commit,
-  lab_description: lab_description,
-  given_course: given_course
-})
-
-labs << FactoryGirl.create(:lab, {
-  active: false,
-  given_course: given_course
-})
-
-#### LabGroup
-lab_group = FactoryGirl.create(:lab_group, {
-  given_course: given_course
-})
-
-lab_group.add_student(student)
-
-#### LabHasGroup
-labs.each_with_index do |lab, index|
-  lhg = FactoryGirl.create(:lab_has_group, {
-    lab: lab, 
-    lab_group: lab_group,
-    repository: FactoryGirl.create(:repo_with_data)
-  })
-
-  FactoryGirl.create(:submission, {
-    commit_hash: commits[index],
-    lab_has_group: lhg
-    #comment: FactoryGirl.create(:comment)
-  })
-end
-
-########################################
-##### Commongood finalizings ###########
-########################################
-
-### DefaultDeadline
-labs.each_with_index do |lab, i|
-  FactoryGirl.create(:default_deadline, {
-    lab: lab,
-    at: ((i + 1) * 2).days.from_now
-  })
-end
-
-#### ExtendedDeadline
-labs.each do |lab|
-  lab.lab_has_groups.each_with_index do |lhg, i|
-    FactoryGirl.create(:extended_deadline, {
-      lab_has_group: lhg,
-      at: ((i + 1) * 5).days.from_now
-    })
-  end
-end
 
