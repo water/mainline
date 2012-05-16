46ca86023f31f49f605ddc371c50fd9dd590b028
Jesper Josefsson <jesper.josefsson@gmail.com>
2012-05-16 12:12:21 +0200

    Adding the student in the create method didn't work, adding it manually [close #168]

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -13,19 +13,19 @@ class RegistrationsController < ApplicationController
 		  @given_course = GivenCourse.find(params[:course_id])
       @lab = @given_course.labs.find(params[:id])
 			@lab_group = LabGroup.create!(
-				given_course: @given_course,
-				students: [current_user.student]
+				given_course: @given_course
 			)
-
+			
+			@lab_group.add_student(current_user.student)
+			
 			repository = Repository.create!()
 			
       # @lab = @given_course.labs.find_by_number(params[:lab_id])
       
-      logger.info(@lab)
 			LabHasGroup.create!(
 				repository: repository, 
 				lab: @lab,
-				lab_group_id: @lab_group
+				lab_group: @lab_group
 			)
 	  end
     rescue  
