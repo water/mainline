b0bb59e4f633af70e749b51a17da97d93ac92653
Jesper Josefsson <jesper.josefsson@gmail.com>
2012-05-26 11:58:14 +0200

    To make things easier for now - all assistants for a given course can review all submissions [#156]

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,7 +3,7 @@ class Ability
   
   def initialize(user)
     if assistant = user.assistant
-      can :review, Submission, lab_has_group: {assistant: assistant}
+      can :review, Submission, lab_has_group: {lab_group: {given_course: assistant.given_courses}}
     end
   end
 end
