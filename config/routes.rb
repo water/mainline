f8bb20ac577e27de69318799e457d8416e8c7fc4
Jesper Josefsson <jesper.josefsson@gmail.com>
2012-06-03 16:23:12 +0200

    Housecleaning in routes #[175]

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,7 @@
 
 Gitorious::Application.routes.draw do
   root to: "dashboards#show"
-  resources :registered_courses, :students
-  resources :lab_deadlines, :study_periods, :course_codes
-
+  
   scope ":role", constraints: { role: /examiner|student|administrator|assistant|/ } do
     resource :dashboard, only: [:show]
     resources :labs
@@ -15,7 +13,7 @@ Gitorious::Application.routes.draw do
           post "/join" => "labs#join"
         end
       end
-      post "/courses/:course_id/upload" => "uploads#upload"
+
       resources :lab_groups do
         collection do
           post "join" => "lab_groups#join"
@@ -65,144 +63,8 @@ Gitorious::Application.routes.draw do
     resources :commit_requests, only: [:create]
   end
   
-  extend Gitorious::RepositoryRoutes
-  
-  resources :merge_requests
-  
-  resources :merge_requests do
-    member do
-      get :version
-      get :commit_status
-      get :terms_accepted
-    end
-  end
-    
-  scope "/+:group_id" do
-    resources :memberships
-  end 
-
-  resources :messages do
-    member do
-      post :reply
-      put :read
-    end
-
-    collection do
-      get :auto_complete_for_message_recipients
-      get :sent
-      put :bulk_update
-      get :all
-    end
-  end
-  
-  resources :users do
-    collection do
-      put :update
-      get :reset_password
-      get :forgot_password
-      get :edit
-    end
-    
-    resources :repositories
-  end
-    
-  match "users/activate/:activation_code" => "users#activate"
-  match "users/pending_activation" => "users#pending_activation"
-  match "users/reset_password/:token" => "users#reset_password", :as => "reset_password"
-
-  match "/merge_request_landing_page" => "merge_requests#oauth_return"
-  match "/merge_requests/:id" => "merge_requests#direct_access"
-
   resource :sessions
   match "/login" => "sessions#new"
   match "/logout" => "sessions#destroy"
 
-  match "/activities" => "site#public_timeline", :as => :activity
-
-  match "/about" => "site#about", :as => :about
-  match "/about/faq" => "site#about", :as => :faq
-  match "/contact" => "site#contact", :as => :contact
-
-  namespace :admin do
-    resources :users do
-      member do
-        put :suspend
-        put :unsuspend
-        put :reset_password
-      end
-    end
-  end
-
-  resources_with_custom_prefix :users, "~" do
-    member do
-      get :feed
-      get :password
-      put :update_password
-      delete :avatar
-      get :watchlist
-    end
-
-    collection do
-      get :forgot_password
-      post :forgot_password_create
-      get :openid_build
-      post :openid_create
-    end
-
-    scope do
-      resources :keys
-      resources :aliases do
-        get :confirm, :on => :member
-      end
-
-      repositories
-
-      resources_with_custom_prefix :projects do
-        repositories
-      end
-    end
-  end
-
-  resources_with_custom_prefix :groups, "+" do
-    delete :avatar, :on => :member
-
-    resources :memberships do
-      get :auto_complete_for_user_login, :on => :collection
-    end
-
-    repositories
-
-    resources_with_custom_prefix :projects do
-      repositories
-    end
-  end
-    
-  resources :groups do
-      resources :repositories
-  end
-     
-  resources :groups do
-      resources :repositories do
-        resources :commits, :trees
-      end
-    resources :repositories
-  end
-    
-  scope "/:user_id" do
-      scope ":repository_id" do
-        match "commit/:id(.:format)" => "commits#show"
-      end
-  end
-   
-  resources :commit_lists do
-    resources :repositories do
-      resources :merge_requests
-    end
-  end
-  
-  resources :versions do
-    resources :repositories do
-      resources :merge_requests
-    end
-  end
 end
