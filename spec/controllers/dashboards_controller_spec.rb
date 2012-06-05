f2dfe61470ff775a2bda29ac7d866122aabcc9bd
Jesper Josefsson <jesper.josefsson@gmail.com>
2012-06-05 20:03:22 +0300

    Add spec for dashboards controller

---
+++ spec/controllers/dashboards_controller_spec.rb
@@ -0,0 +1,37 @@
+describe DashboardsController do
+  render_views
+  
+  describe "Student dashboard" do
+    let(:course2) { FactoryGirl.create(:given_course)}
+    
+    before :all do
+      @student = FactoryGirl.create(:student_registered_for_course).student
+      @group = FactoryGirl.create(:lab_group, given_course: @student.given_courses.first)
+      @lab = FactoryGirl.create(:lab, given_course: @student.given_courses.first)
+      @group.add_student(@student)
+      @lab.add_group!(@group)
+    end
+    
+    before :each do 
+      login_as(@student)
+      visit dashboard_path(role: "student")
+    end
+    
+    it "shows a particular course" do
+      page.should have_content(@lab.given_course.course.course_codes.first.code)
+    end
+    
+    it "shows two courses" do
+      course2.register_student(@student)
+      page.should have_content(@lab.given_course.course.course_codes.first.code)
+      page.should have_content(course2.course.course_codes.first.code)
+    end
+    
+    it "shows a lab" do
+      page.should have_content(@lab.name)
+    end
+  end
+  
+  describe "Assistant" do
+  end
+end
