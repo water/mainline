bdd378dd7ebe1f44aab2c0bedf7cea13971bd335
Arash Rouhani <miffoljud@gmail.com>
2012-06-10 19:08:36 +0200

    Fix TODO in grack specs

--- spec/grack/water_grack_auth_spec.rb
+++ spec/grack/water_grack_auth_spec.rb
@@ -32,13 +32,8 @@ describe "WaterGrackAuth" do
   let(:given_course) { lab_group.given_course }
   let(:url) { make_url(given_course, lab, lab_group) }
 
-  # TODO: use definition from LabHasGroup model
   def make_url(given_course, lab, lab_group)
-    %W{courses/#{given_course.id}/
-       labs/#{lab.number}/
-       lab_groups/#{lab_group.number}
-       .git
-    }.join ""
+    LabHasGroup.build_repo_qualifier(given_course.id, lab.number, lab_group.number)
   end
 
   before(:each) do
