040980a5ff7a6ad30860c8ef9626e308d96c35a6
Arash Rouhani <miffoljud@gmail.com>
2012-06-17 20:13:42 +0200

    Run migrations

--- db/schema.rb
+++ db/schema.rb
@@ -291,15 +291,6 @@ ActiveRecord::Schema.define(:version => 20120617122643) do
     t.string  "salt",       :null => false
   end
 
-  create_table "queue_classic_jobs", :force => true do |t|
-    t.string   "q_name"
-    t.string   "method"
-    t.text     "args"
-    t.datetime "locked_at"
-  end
-
-  add_index "queue_classic_jobs", ["q_name", "id"], :name => "idx_qc_on_name_only_unlocked"
-
   create_table "repositories", :force => true do |t|
     t.datetime "created_at",                     :null => false
     t.datetime "updated_at",                     :null => false
