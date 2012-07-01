eaff4efbe2ab30f5de471a32c301f1839c52d1b3
Arash Rouhani <miffoljud@gmail.com>
2012-07-01 14:03:50 +0200

    Add, rename and refactor submission specs

--- spec/models/submission_spec.rb
+++ spec/models/submission_spec.rb
@@ -69,7 +69,7 @@ describe Submission do
         commit_hash: nil,
         lab_has_group: lhg
       })
-      
+
       s.commit_hash.should == repository.head_candidate.commit
     end
 
@@ -91,28 +91,59 @@ describe Submission do
   end
 
   describe "states" do
-    it "should not be possible to create a submission when LabHasGroup is in state 'accepted'" do
-      lhg = build(:lab_has_group, state: "accepted")
-      build(:submission, lab_has_group: lhg).should_not be_valid
-    end
+    describe "creation rejects" do
+      it "should not be possible to create a submission from state accepted" do
+        lhg = build(:lab_has_group, state: "accepted")
+        build(:submission, lab_has_group: lhg).should_not be_valid
+      end
 
-    it "should not be possible to create a submission when LabHasGroup is in state 'reviewing'" do
-      lhg = build(:lab_has_group, state: "reviewing")
-      build(:submission, lab_has_group: lhg).should_not be_valid
+      it "should not be possible to create a submission from state reviewing" do
+        lhg = build(:lab_has_group, state: "reviewing")
+        build(:submission, lab_has_group: lhg).should_not be_valid
+      end
+
+      it "should not be possible to create a submission from state pending" do
+        lhg = build(:lab_has_group, state: "pending")
+        build(:submission, lab_has_group: lhg).should_not be_valid
+      end
+    end
+    describe "update rejects" do
+      it "should not be possible to update a submission from state reviewing" do
+        lhg = build(:lab_has_group, state: "reviewing")
+        submission = build(:submission, lab_has_group: lhg)
+        submission.update_attributes({}).should be_false
+      end
+
+      it "should be ok to update a pending submission" do
+        lhg = build(:lab_has_group, state: "initialized")
+        submission = create(:submission, lab_has_group: lhg)
+        # by now lhg is pending since we used 'create' which does 'after_save'
+        submission.update_attributes({}).should be_true
+      end
     end
 
     it "should change state from 'initialized' to 'pending' on create" do
       lhg = create(:lab_has_group)
       create(:submission, lab_has_group: lhg)
       lhg.should be_pending
+      lhg.should_not be_changed
+    end
+
+    it "should change state from 'initialized' to 'pending' on save" do
+      real_hash = "cc6e8cd7426322681baa4afb6f3708cbff41a697" # From fixture
+      lhg = create(:lab_has_group)
+      submission = Submission.new(lab_has_group: lhg,
+                                  commit_hash:   real_hash)
+      submission.save
+      lhg.should be_pending
     end
 
     it "should keep state 'pending' on update" do
       lhg = create(:lab_has_group)
-      old_hash = "cc6e8cd7426322681baa4afb6f3708cbff41a697"
-      new_hash = "286e790e4b9244147e2a99e0f1a05ba121fbec88"
+      old_hash = "cc6e8cd7426322681baa4afb6f3708cbff41a697" # From fixture
+      new_hash = "286e790e4b9244147e2a99e0f1a05ba121fbec88" # From fixture
       submission = create(:submission, lab_has_group: lhg, commit_hash: old_hash)
-      submission.update_attribute(:commit_hash, new_hash)
+      submission.update_attributes(commit_hash: new_hash).should be_true
       lhg.should be_pending
     end
 
@@ -120,11 +151,6 @@ describe Submission do
       lhg = create(:lab_has_group, state: "rejected")
       create(:submission, lab_has_group: lhg)
       lhg.should be_pending
-    end
-
-    it "should not be possible to create a submission when LabHasGroup is in state 'pending'" do
-      lhg = build(:lab_has_group, state: "pending")
-      build(:submission, lab_has_group: lhg).should_not be_valid
     end
   end
 end
